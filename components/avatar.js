29581e4af2b8c9e4fbccc1cba858726e75fe2b02
Silas <krausesilas@gmail.com>
2020-08-28 23:35:40 +0200

    Adjust data fetching

--- components/avatar.js
+++ components/avatar.js
@@ -1,14 +1,11 @@
 export default function Avatar({ author }) {
-  const name =
-    author.firstName && author.lastName
-      ? `${author.firstName} ${author.lastName}`
-      : author.name
-
+  const name = author[0]?.title
+  const baseURL = "http://mosaik.test/assets/main/profile/"
   return (
     <div className="flex items-center">
       <img
-        src={author.avatar.url}
-        className="w-12 h-12 rounded-full mr-4"
+        src={`${baseURL + author[0]?.slug}.jpg`}
+        className="w-12 h-12 rounded-full mr-4 object-cover"
         alt={name}
       />
       <div className="text-xl font-bold">{name}</div>
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,34 @@
 import Head from 'next/head'
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
+import PostPreview from '../components/post-preview'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
 
-export default function Index({ allPosts: { edges }, preview }) {
-  const heroPost = edges[0]?.node
-  const morePosts = edges.slice(1)
-
+export default function Index({ allPosts, preview }) {
   return (
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Mosaik Academy - Blog</title>
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.featuredImage.node}
-              date={heroPost.date}
-              author={heroPost.author.node}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          <section>
+            <div className="flex flex-col mb-32">
+              {allPosts.map(( post ) => (
+                <PostPreview
+                  key={post.slug}
+                  title={post.title}
+                  coverImage={post.cover_image}
+                  date={post.date}
+                  author={post.author}
+                  slug={post.slug}
+                  //excerpt={node.excerpt}
+                />
+              ))}
+            </div>
+          </section>
         </Container>
       </Layout>
     </>
