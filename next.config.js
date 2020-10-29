07ef9efd4f9b0a25cf0f56b5023a0d4ac46da045
Silas <krausesilas@gmail.com>
2020-10-29 21:58:27 +0100

    Add basePath to image path

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     images: {
         //deviceSizes: [320, 420, 768, 1024, 1200],
         domains: ['mosaik.test', 'mosaik-stuttgart.net'],
-        path: '/_next/image',
+        path: process.env.BASE_URL + '/_next/image',
         loader: 'default',
       },
 }
