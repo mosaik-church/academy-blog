2d75db91265067bfb266ee3e8ab49e54565bb5df
Silas <krausesilas@gmail.com>
2020-10-29 23:19:33 +0100

    Add baseUrl to meta

--- components/meta.js
+++ components/meta.js
@@ -2,36 +2,37 @@ import Head from 'next/head'
 import { HOME_OG_IMAGE_URL } from '../lib/constants'
 
 export default function Meta() {
+  function baseUrl(path) { return '/blog' + path}
   return (
     <Head>
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href={baseUrl("/favicon/apple-touch-icon.png")}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href={baseUrl("/favicon/favicon-32x32.png")}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href={("/favicon/favicon-16x16.png")}
       />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="manifest" href={baseUrl("/site.webmanifest")} />
       <link
         rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
+        href={baseUrl("/favicon/safari-pinned-tab.svg")}
         color="#377e70"
       />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="shortcut icon" href={baseUrl("/favicon/favicon.ico")} />
       <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="msapplication-config" content={baseUrl("/favicon/browserconfig.xml")} />
       <meta name="theme-color" content="#E4EAE4" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <link rel="alternate" type="application/rss+xml" href={baseUrl("/feed.xml")} />
       <meta
         name="description"
         content="Der Mosaik Academy Blog soll dir dabei helfen im Glauben zu wachsen, die Bibel besser zu verstehen und deine Berufung zu leben."
