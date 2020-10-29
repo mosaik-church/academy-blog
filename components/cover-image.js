f8c00e1a0373b17a0486644e134f39184389a128
Silas <krausesilas@gmail.com>
2020-10-29 22:06:41 +0100

    Adjust image size

--- components/cover-image.js
+++ components/cover-image.js
@@ -2,14 +2,8 @@ import Image from 'next/image'
 
 export default function CoverImage({ title, coverImage, slug }) {
 
-  const attr = {
-    height: slug ? '400' : false,
-    width: slug ? '600' : false,
-    unsized: slug ? false : true
-  }
-
   const image = (
-    coverImage && <Image {...attr} alt={title} className="cover-image" src={coverImage?.permalink} />
+    coverImage && <Image unsized alt={title} className="cover-image" src={coverImage?.permalink} />
   )
   return (
     <div className={slug ? '' : '-mx-4 md:mx-0'} >
