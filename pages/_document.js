640fff06cc9bc402e2f6787caaaf1aacc0512973
Silas <krausesilas@gmail.com>
2020-10-31 13:31:03 +0100

    Fix baseUrl

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="de">
         <Head>
-          <link rel="preload" href="/fonts/source-sans-pro-light.woff2" as="font"></link>
-          <link rel="preload" href="/fonts/source-sans-pro-regular.woff2" as="font"></link>
-          <link rel="preload" href="/fonts/source-sans-pro-semibold.woff2" as="font"></link>
-          <link rel="preload" href="/fonts/source-sans-pro-bold.woff2" as="font"></link>
+          <link rel="preload" href="/blog/fonts/source-sans-pro-light.woff2" as="font"></link>
+          <link rel="preload" href="/blog/fonts/source-sans-pro-regular.woff2" as="font"></link>
+          <link rel="preload" href="/blog/fonts/source-sans-pro-semibold.woff2" as="font"></link>
+          <link rel="preload" href="/blog/fonts/source-sans-pro-bold.woff2" as="font"></link>
         </Head>
         <body>
           <Main />
