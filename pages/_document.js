95e214430104d61b328f14863e0af44d1ac1a171
Silas <krausesilas@gmail.com>
2020-10-31 13:08:55 +0100

    Preload google font stylesheet

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,10 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="de">
-        <Head />
+        <Head>
+          <link rel="preload" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700&display=swap" as="style"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
+        </Head>
         <body>
           <Main />
           <NextScript />
